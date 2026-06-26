335d89b6886319810ad74197af4ca7cbd6a9ceef
kabirz <jxwazxzhp@126.com>
2026-06-26 16:04:00 +0800

    feat: add Linux memory barriers deep analysis article

--- common.js
+++ common.js
@@ -22,6 +22,7 @@
     { t:"Linux 工作队列", f:"linux-workqueue.html", top:"linux" },
     { t:"Linux WQ_BH", f:"linux-wq-bh.html", top:"linux" },
     { t:"Linux WRITE_ONCE/READ_ONCE", f:"linux-rwonce.html", top:"linux" },
+    { t:"Linux 内存屏障", f:"linux-membarrier.html", top:"linux" },
     { t:"Linux cgroup", f:"linux-cgroup.html", top:"linux" },
     { t:"Linux cleanup.h", f:"linux-cleanup.html", top:"linux" },
     { t:"Linux seq_file", f:"linux-seq_file.html", top:"linux" },
